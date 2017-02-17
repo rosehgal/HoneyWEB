d9e45555286bcaf86b6a97d3de479374c336ea37
Rohit Sehgal <rohitsehgal1994@gmail.com>
2017-02-17 10:02:08 +0530

    Commit System ready

--- proxyDocker/proxy.js
+++ proxyDocker/proxy.js
@@ -1,8 +1,8 @@
 var httpProxy = require("http-proxy");
 var http = require("http");
 
-var proxy = httpProxy.createProxyServer({target:'http://0.0.0.0:80'}).listen(80);
+var proxy = httpProxy.createProxyServer({target:'http://server_1:80'}).listen(80);
 
 proxy.on('proxyReq',function(proxyReq,req,res){
-  console.log("got a connection");
+  console.log("got a connection"+ typeof(req));
 });
