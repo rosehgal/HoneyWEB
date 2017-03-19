80d9a39fa9179a7398eb28093982f2c6e4163496
Rohit Sehgal <rsehgal@iitk.ac.in>
2017-03-19 11:25:12 +0530

    Revert "changes in log structure"
    This reverts commit 4dac43a85456ba53a571b49ff1560e49eed2469e.

--- proxyDocker/startup_script.sh
+++ proxyDocker/startup_script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-tcpdump -A -s 0 'tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' -G 3600 -w /app/proxy/logs/"capture_+%Y-%m-%d.pcap" &
+tcpdump -A -s 0 'tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)' -w /app/proxy/logs/capture.pcap &
 npm start
